de07c6c8756f38c3b6eaf0699069527c98ac682d
Kayode Olaniyi <ifedamilola2009@gmail.com>
2022-05-21 12:38:28 +0100

    finished deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
-git pull
-npm install
-ng build --prod
- pm2 start "ng serve"
+git pull 
+cp src/environments/environment.prod.ts src/environments/environment.ts
+docker build -t emc-portal/angular-app:latest . 
+docker stop $(docker ps -a -q)
+docker run -d -p 4200:80 emc-portal/angular-app:latest
+ 
