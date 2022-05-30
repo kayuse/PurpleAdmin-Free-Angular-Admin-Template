36ee6a9aefd9d2864ff9111a0ce07917bfcaea1d
Kayode Olaniyi <ifedamilola2009@gmail.com>
2022-05-30 19:26:28 +0100

    Update push_deploy.sh

--- push_deploy.sh
+++ push_deploy.sh
@@ -1,4 +1,4 @@
  #!/bin/bash
 cp src/environments/environment.prod.ts src/environments/environment.ts
-docker build -t ilanaasoft/emc-portal:latest  . 
+docker build -t ilanaasoft/emc-portal:latest  .   --platform linux/amd64
 docker push ilanaasoft/emc-portal:latest
