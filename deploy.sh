4c4e4fb45b0077c2683207f3266a036c8390d411
Kayode Olaniyi <ifedamilola2009@gmail.com>
2022-05-30 17:35:14 +0100

    added docker pull

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,9 @@
 git pull 
 cp src/environments/environment.prod.ts src/environments/environment.ts
+docker pull ilanaasoft/emc-portal:latest
 # ng build --prod
 # pm2 start
-docker build -t emc-portal/angular-app:latest . 
-docker stop $(docker ps -a -q)
-docker run -d -p 4200:80 emc-portal/angular-app:latest
+# docker build -t emc-portal/angular-app:latest . 
+# docker stop $(docker ps -a -q)
+# docker run -d -p 4200:80 emc-portal/angular-app:latest
  
